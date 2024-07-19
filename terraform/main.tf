164ff1a69f89d6858dfe4efaaa59fe07377ae683
David Yeo <david.j.yeo@outlook.com>
2024-07-19 15:46:26 +0100

    Build: Initial Deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -85,134 +85,134 @@ resource "azurerm_virtual_network_peering" "spoke-to-hub" {
 }
 
 
-# resource "azurerm_public_ip" "pip_azfw" {
-#   name                = module.naming.public_ip.name
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   allocation_method   = "Static"
-#   sku                 = "Standard"
-# }
+resource "azurerm_public_ip" "pip_azfw" {
+  name                = module.naming.public_ip.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
 
-# resource "azurerm_firewall" "azfw" {
-#   name                = module.naming.firewall.name
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   sku_name            = "AZFW_VNet"
-#   sku_tier            = "Standard"
-#   ip_configuration {
-#     name                 = module.naming.firewall_ip_configuration.name
-#     subnet_id            = azurerm_subnet.firewall_subnet.id
-#     public_ip_address_id = azurerm_public_ip.pip_azfw.id
-#   }
-# }
+resource "azurerm_firewall" "azfw" {
+  name                = module.naming.firewall.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+  ip_configuration {
+    name                 = module.naming.firewall_ip_configuration.name
+    subnet_id            = azurerm_subnet.firewall_subnet.id
+    public_ip_address_id = azurerm_public_ip.pip_azfw.id
+  }
+}
 
-# resource "azurerm_firewall_policy" "azfw" {
-#   name                = module.naming.firewall_policy.name
-#   resource_group_name = azurerm_resource_group.rg.name
-#   location            = azurerm_resource_group.rg.location
-# }
+resource "azurerm_firewall_policy" "azfw" {
+  name                = module.naming.firewall_policy.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
 
-# resource "azurerm_route_table" "rt" {
-#   name                          = module.naming.route_table.name
-#   location                      = azurerm_resource_group.rg.location
-#   resource_group_name           = azurerm_resource_group.rg.name
-#   disable_bgp_route_propagation = false
-#   route {
-#     name                   = module.naming.route.name
-#     address_prefix         = "0.0.0.0/0"
-#     next_hop_type          = "VirtualAppliance"
-#     next_hop_in_ip_address = azurerm_firewall.azfw.ip_configuration[0].private_ip_address
-#   }
-# }
+resource "azurerm_route_table" "rt" {
+  name                          = module.naming.route_table.name
+  location                      = azurerm_resource_group.rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  disable_bgp_route_propagation = false
+  route {
+    name                   = module.naming.route.name
+    address_prefix         = "0.0.0.0/0"
+    next_hop_type          = "VirtualAppliance"
+    next_hop_in_ip_address = azurerm_firewall.azfw.ip_configuration[0].private_ip_address
+  }
+}
 
-# resource "azurerm_subnet_route_table_association" "subnet_rt_association" {
-#   subnet_id      = azurerm_subnet.SN-Workload.id
-#   route_table_id = azurerm_route_table.rt.id
-# }
+resource "azurerm_subnet_route_table_association" "subnet_rt_association" {
+  subnet_id      = azurerm_subnet.SN-Workload.id
+  route_table_id = azurerm_route_table.rt.id
+}
 
-# resource "azurerm_subnet_route_table_association" "ansible_subnet_rt_association" {
-#   subnet_id      = azurerm_subnet.ansible_subnet.id
-#   route_table_id = azurerm_route_table.rt.id
-# }
+resource "azurerm_subnet_route_table_association" "ansible_subnet_rt_association" {
+  subnet_id      = azurerm_subnet.ansible_subnet.id
+  route_table_id = azurerm_route_table.rt.id
+}
 
-# resource "azurerm_firewall_nat_rule_collection" "nat_rule_collection" {
-#   name                = module.naming.firewall_nat_rule_collection.name
-#   azure_firewall_name = azurerm_firewall.azfw.name
-#   resource_group_name = azurerm_resource_group.rg.name
-#   priority            = 200
-#   action              = "Dnat"
+resource "azurerm_firewall_nat_rule_collection" "nat_rule_collection" {
+  name                = module.naming.firewall_nat_rule_collection.name
+  azure_firewall_name = azurerm_firewall.azfw.name
+  resource_group_name = azurerm_resource_group.rg.name
+  priority            = 200
+  action              = "Dnat"
 
-#   rule {
-#     name = "rdp-nat"
-#     source_addresses = [
-#       "*"
-#     ]
+  rule {
+    name = "rdp-nat"
+    source_addresses = [
+      "*"
+    ]
 
-#     destination_ports = [
-#       "3389"
-#     ]
+    destination_ports = [
+      "3389"
+    ]
 
-#     destination_addresses = [
-#       azurerm_public_ip.pip_azfw.ip_address
-#     ]
+    destination_addresses = [
+      azurerm_public_ip.pip_azfw.ip_address
+    ]
 
-#     translated_port    = 3389
-#     translated_address = module.dc01.network_interfaces.network_interface_1.private_ip_address
-#     protocols = [
-#       "TCP"
-#     ]
-#   }
-#   rule {
-#     name = "ssh-nat"
-#     source_addresses = [
-#       "*"
-#     ]
+    translated_port    = 3389
+    translated_address = module.dc01.network_interfaces.network_interface_1.private_ip_address
+    protocols = [
+      "TCP"
+    ]
+  }
+  rule {
+    name = "ssh-nat"
+    source_addresses = [
+      "*"
+    ]
 
-#     destination_ports = [
-#       "22"
-#     ]
+    destination_ports = [
+      "22"
+    ]
 
-#     destination_addresses = [
-#       azurerm_public_ip.pip_azfw.ip_address
-#     ]
+    destination_addresses = [
+      azurerm_public_ip.pip_azfw.ip_address
+    ]
 
-#     translated_port    = 22
-#     translated_address = module.control.network_interfaces.network_interface_1.private_ip_address
-#     protocols = [
-#       "TCP"
-#     ]
-#   }
+    translated_port    = 22
+    translated_address = module.control.network_interfaces.network_interface_1.private_ip_address
+    protocols = [
+      "TCP"
+    ]
+  }
 
-#   depends_on = [
-#     module.dc01,
-#     module.control
-#   ]
-# }
+  depends_on = [
+    module.dc01,
+    module.control
+  ]
+}
 
-# resource "azurerm_firewall_network_rule_collection" "example" {
-#   name                = module.naming.firewall_network_rule_collection.name
-#   azure_firewall_name = azurerm_firewall.azfw.name
-#   resource_group_name = azurerm_resource_group.rg.name
-#   priority            = 200
-#   action              = "Allow"
+resource "azurerm_firewall_network_rule_collection" "example" {
+  name                = module.naming.firewall_network_rule_collection.name
+  azure_firewall_name = azurerm_firewall.azfw.name
+  resource_group_name = azurerm_resource_group.rg.name
+  priority            = 200
+  action              = "Allow"
 
-#   rule {
-#     name = "OutBoundAllAll"
+  rule {
+    name = "OutBoundAllAll"
 
-#     source_addresses = [
-#       "*",
-#     ]
+    source_addresses = [
+      "*",
+    ]
 
-#     destination_ports = [
-#       "*",
-#     ]
+    destination_ports = [
+      "*",
+    ]
 
-#     destination_addresses = [
-#       "*"
-#     ]
+    destination_addresses = [
+      "*"
+    ]
 
-#     protocols = [
-#       "Any"
-#     ]
-#   }
-# }
+    protocols = [
+      "Any"
+    ]
+  }
+}
