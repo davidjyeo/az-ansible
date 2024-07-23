d3393f0d7738e68c8c083988209870b1d43e65c8
David Yeo <david.j.yeo@outlook.com>
2024-07-23 08:34:18 +0100

    build(ssh_key): API Verison

--- terraform/ssh.tf
+++ terraform/ssh.tf
@@ -1,5 +1,5 @@
 resource "azapi_resource_action" "ssh_public_key_gen" {
-  type                   = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  type                   = "Microsoft.Compute/sshPublicKeys@2023-09-01"
   resource_id            = azapi_resource.ssh_public_key.id
   action                 = "generateKeyPair"
   method                 = "POST"
@@ -7,7 +7,7 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
 }
 
 resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  type      = "Microsoft.Compute/sshPublicKeys@2023-09-01"
   name      = "linux-ssh-public-key"
   location  = azurerm_resource_group.rg.location
   parent_id = azurerm_resource_group.rg.id
