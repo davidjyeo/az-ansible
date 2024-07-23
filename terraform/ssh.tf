31435e8f3d291ef531b87369347dd42bfe663431
David Yeo <david.j.yeo@outlook.com>
2024-07-23 13:43:29 +0100

    build(vm): Ubuntu - Control Server

--- terraform/ssh.tf
+++ terraform/ssh.tf
@@ -14,12 +14,12 @@ resource "azurerm_key_vault_secret" "admin_ssh_key" {
 }
 
 
-resource "azurerm_key_vault_secret" "admin_ssh_public_key" {
-  key_vault_id = module.avm-res-keyvault-vault.resource_id
-  name         = "azureuser-ssh-public-key"
-  value        = tls_private_key.ssh.public_key_pem
+# resource "azurerm_key_vault_secret" "admin_ssh_public_key" {
+#   key_vault_id = module.avm-res-keyvault-vault.resource_id
+#   name         = "azureuser-ssh-public-key"
+#   value        = tls_private_key.ssh.public_key_pem
 
-  depends_on = [
-    module.avm-res-keyvault-vault
-  ]
-}
+#   depends_on = [
+#     module.avm-res-keyvault-vault
+#   ]
+# }
