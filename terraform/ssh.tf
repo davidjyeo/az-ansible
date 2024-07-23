97de0084198edf4ec26d864cc9d916beecc9e557
David Yeo <david.j.yeo@outlook.com>
2024-07-23 12:54:02 +0100

    build(Keyvault): SSH Generation

--- terraform/ssh.tf
+++ terraform/ssh.tf
@@ -6,7 +6,7 @@ resource "tls_private_key" "ssh" {
 resource "azurerm_key_vault_secret" "admin_ssh_key" {
   key_vault_id = module.avm_res_keyvault_vault.resource_id
   name         = "azureuser-ssh-private-key"
-  value        = tls_private_key.this.private_key_pem
+  value        = tls_private_key.ssh.private_key_pem
 
   depends_on = [
     module.avm_res_keyvault_vault
