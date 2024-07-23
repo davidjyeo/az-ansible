c4a4dfc577f7313cf67abe10f2e2eff63e4a9a45
David Yeo <david.j.yeo@outlook.com>
2024-07-23 16:04:55 +0100

    build(DSC): Domain Setup

--- terraform/automation_account.tf
+++ terraform/automation_account.tf
@@ -10,5 +10,26 @@ resource "azurerm_automation_account" "aa" {
       azurerm_user_assigned_identity.uai.id
     ]
   }
+}
 
+# ActiveDirectoryDsc
+resource "azurerm_automation_module" "example" {
+  name                    = "xActiveDirectory"
+  resource_group_name     = azurerm_resource_group.rg.name
+  automation_account_name = azurerm_automation_account.aa.name
+
+  module_link {
+    uri = "https://psg-prod-eastus.azureedge.net/packages/activedirectorydsc.6.5.0.nupkg"
+  }
+}
+
+
+
+
+resource "azurerm_automation_dsc_configuration" "domain" {
+  name                    = "domain_controller"
+  resource_group_name     = azurerm_resource_group.rg.name
+  automation_account_name = azurerm_automation_account.aa.name
+  location                = azurerm_resource_group.rg.location
+  content_embedded        = file("../scripts/dc01.ps1")
 }
