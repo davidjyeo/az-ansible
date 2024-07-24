0d08cbe71b754dee55b8e978ddfe9efc26acef27
David Yeo <david.j.yeo@outlook.com>
2024-07-24 09:24:39 +0100

    build(azfw): policy rule collection group

--- terraform/main.tf
+++ terraform/main.tf
@@ -220,3 +220,64 @@ resource "azurerm_firewall_network_rule_collection" "network_rule_collection_out
     ]
   }
 }
+
+resource "azurerm_firewall_policy_rule_collection_group" "rule_collecton" {
+  name               = module.naming.firewall_policy_rule_collection_group.name
+  firewall_policy_id = azurerm_firewall_policy.azfw.id
+  priority           = 500
+  # application_rule_collection {
+  #   name     = "app_rule_collection1"
+  #   priority = 500
+  #   action   = "Deny"
+  #   rule {
+  #     name = "app_rule_collection1_rule1"
+  #     protocols {
+  #       type = "Http"
+  #       port = 80
+  #     }
+  #     protocols {
+  #       type = "Https"
+  #       port = 443
+  #     }
+  #     source_addresses  = ["10.0.0.1"]
+  #     destination_fqdns = ["*.microsoft.com"]
+  #   }
+  # }
+
+  network_rule_collection {
+    name     = module.naming.firewall_network_rule_collection.name
+    priority = 200
+    action   = "Allow"
+    rule {
+      name                  = "OutBoundAllAll"
+      protocols             = ["Any"]
+      source_addresses      = ["*"]
+      destination_addresses = ["*"]
+      destination_ports     = ["*"]
+    }
+  }
+
+  nat_rule_collection {
+    name     = "module.naming.firewall_nat_rule_collection.name"
+    priority = 200
+    action   = "Dnat"
+    rule {
+      name                = "rdp-nat"
+      protocols           = ["TCP"]
+      source_addresses    = ["*"]
+      destination_address = azurerm_public_ip.pip_azfw.ip_address
+      destination_ports   = ["3389"]
+      translated_address  = module.dc01.network_interfaces.network_interface_1.private_ip_address
+      translated_port     = "3389"
+    }
+    rule {
+      name                = "ssh-nat"
+      protocols           = ["TCP"]
+      source_addresses    = ["*"]
+      destination_address = azurerm_public_ip.pip_azfw.ip_address
+      destination_ports   = ["22"]
+      translated_address  = module.control.network_interfaces.network_interface_1.private_ip_address
+      translated_port     = "22"
+    }
+  }
+}
