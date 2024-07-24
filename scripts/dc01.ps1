fd1f755df1199ff2e3df3aff3d84856e427f91d3
David Yeo <david.j.yeo@outlook.com>
2024-07-24 08:00:02 +0100

    build(dsc): Automation Account Credential

--- scripts/dc01.ps1
+++ scripts/dc01.ps1
@@ -1,23 +1,38 @@
 Configuration dc01
 {
-  $domainCred = Get-AutomationPSCredential -Name "DomainAdmin"
-  $domainName = Get-AutomationVariable -Name "ansible"
-  $domainDN = Get-AutomationVariable -Name "DomainDN"
+  $domainCred = Get-AutomationPSCredential -Name "localmgr"
+  $domainName = Get-AutomationVariable -Name "ansible-poc"
+  $domainDN = Get-AutomationVariable -Name "DC=ansible-poc,DC=local"
 	
-  # Import the modules needed to run the DSC script
-  Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
-  Import-DScResource -ModuleName 'ComputerManagementDsc'
-  Import-DscResource -ModuleName 'ActiveDirectoryDsc'	
+  param
+  (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [System.Management.Automation.PSCredential]
+    $Credential,
 
-  Node "localhost"
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [System.Management.Automation.PSCredential]
+    $SafeModePassword
+  )
+  
+  
+  
+  # Import the modules needed to run the DSC script
+  Import-DscResource -ModuleName PSDesiredStateConfiguration
+  Import-DScResource -ModuleName ComputerManagementDsc
+  Import-DscResource -ModuleName ActiveDirectoryDsc
+
+  Node localhost
+
   {
-    WindowsFeature ADDSInstall {
-      Ensure    = "Present"
-      Name      = "AD-Domain-Services"
-      DependsOn = "[Computer]NewComputerName"
+    WindowsFeature ADDS {
+      Ensure = "Present"
+      Name   = "AD-Domain-Services"
     }
 
-    WindowsFeature ADDSTools {
+    WindowsFeature RSAT {
       Ensure = "Present"
       Name   = "RSAT-ADDS"
     }
@@ -27,105 +42,90 @@ Configuration dc01
       Name   = "RSAT-AD-PowerShell"
     }
 		
-    ADDomain $DomainName {
-      DomainName                    = $DomainName
+    ADDomain $domainName {
+      DomainName                    = $domainName
       Credential                    = $domainCred
       SafemodeAdministratorPassword = $domainCred
       ForestMode                    = 'WinThreshold'
       DependsOn                     = "[WindowsFeature]ADDSInstall"
     }	
 
-    WaitForADDomain $DomainName {
-      DomainName           = $DomainName
+    WaitForADDomain $domainName {
+      DomainName           = $domainName
       WaitTimeout          = 600
       RestartCount         = 2
       PsDscRunAsCredential = $domainCred
     }
 
-    ADOrganizationalUnit 'Demo' {
-      Name                            = "Demo"
-      Path                            = "$domainDN"
-      ProtectedFromAccidentalDeletion = $true
-      Description                     = "TopLevel OU"
-      Ensure                          = 'Present'
-    }
+    # ADOrganizationalUnit 'Demo' {
+    #   Name                            = "Demo"
+    #   Path                            = "$domainDN"
+    #   ProtectedFromAccidentalDeletion = $true
+    #   Description                     = "TopLevel OU"
+    #   Ensure                          = 'Present'
+    # }
 		
-    ADOrganizationalUnit 'WebServers' {
-      Name                            = "WebServers"
-      Path                            = "OU=Demo,$domainDN"
-      ProtectedFromAccidentalDeletion = $true
-      Description                     = "WebServers OU"
-      Ensure                          = 'Present'
-      DependsOn                       = "[ADOrganizationalUnit]Demo"
-    }
+    # ADOrganizationalUnit 'WebServers' {
+    #   Name                            = "WebServers"
+    #   Path                            = "OU=Demo,$domainDN"
+    #   ProtectedFromAccidentalDeletion = $true
+    #   Description                     = "WebServers OU"
+    #   Ensure                          = 'Present'
+    #   DependsOn                       = "[ADOrganizationalUnit]Demo"
+    # }
 
-    ADOrganizationalUnit 'Administration' {
-      Name                            = "Administration"
-      Path                            = "OU=Demo,$domainDN"
-      ProtectedFromAccidentalDeletion = $true
-      Description                     = "Administration OU"
-      Ensure                          = 'Present'
-      DependsOn                       = "[ADOrganizationalUnit]Demo"
-    }
+    # ADOrganizationalUnit 'Administration' {
+    #   Name                            = "Administration"
+    #   Path                            = "OU=Demo,$domainDN"
+    #   ProtectedFromAccidentalDeletion = $true
+    #   Description                     = "Administration OU"
+    #   Ensure                          = 'Present'
+    #   DependsOn                       = "[ADOrganizationalUnit]Demo"
+    # }
 
-    ADOrganizationalUnit 'AdminUsers' {
-      Name                            = "AdminUsers"
-      Path                            = "OU=Administration,OU=Demo,$domainDN"
-      ProtectedFromAccidentalDeletion = $true
-      Description                     = "Administration OU"
-      Ensure                          = 'Present'
-      DependsOn                       = "[ADOrganizationalUnit]Administration"
-    }
+    # ADOrganizationalUnit 'AdminUsers' {
+    #   Name                            = "AdminUsers"
+    #   Path                            = "OU=Administration,OU=Demo,$domainDN"
+    #   ProtectedFromAccidentalDeletion = $true
+    #   Description                     = "Administration OU"
+    #   Ensure                          = 'Present'
+    #   DependsOn                       = "[ADOrganizationalUnit]Administration"
+    # }
 
-    ADOrganizationalUnit 'ServiceAccounts' {
-      Name                            = "ServiceAccounts"
-      Path                            = "OU=Demo,$domainDN"
-      ProtectedFromAccidentalDeletion = $true
-      Description                     = "ServiceAccounts OU"
-      Ensure                          = 'Present'
-      DependsOn                       = "[ADOrganizationalUnit]Demo"
-    }
+    # ADOrganizationalUnit 'ServiceAccounts' {
+    #   Name                            = "ServiceAccounts"
+    #   Path                            = "OU=Demo,$domainDN"
+    #   ProtectedFromAccidentalDeletion = $true
+    #   Description                     = "ServiceAccounts OU"
+    #   Ensure                          = 'Present'
+    #   DependsOn                       = "[ADOrganizationalUnit]Demo"
+    # }
 
-    ADOrganizationalUnit 'Citrix' {
-      Name                            = "Citrix"
-      Path                            = "OU=Demo,$domainDN"
-      ProtectedFromAccidentalDeletion = $true
-      Description                     = "Citrix OU"
-      Ensure                          = 'Present'
-      DependsOn                       = "[ADOrganizationalUnit]Demo"
-    }		
+    # ADOrganizationalUnit 'Citrix' {
+    #   Name                            = "Citrix"
+    #   Path                            = "OU=Demo,$domainDN"
+    #   ProtectedFromAccidentalDeletion = $true
+    #   Description                     = "Citrix OU"
+    #   Ensure                          = 'Present'
+    #   DependsOn                       = "[ADOrganizationalUnit]Demo"
+    # }		
     
-    ADOrganizationalUnit 'Users' {
-      Name                            = "Users"
-      Path                            = "OU=Demo,$domainDN"
-      ProtectedFromAccidentalDeletion = $true
-      Description                     = "Users OU"
-      Ensure                          = 'Present'
-      DependsOn                       = "[ADOrganizationalUnit]Demo"
-    }
+    # ADOrganizationalUnit 'Users' {
+    #   Name                            = "Users"
+    #   Path                            = "OU=Demo,$domainDN"
+    #   ProtectedFromAccidentalDeletion = $true
+    #   Description                     = "Users OU"
+    #   Ensure                          = 'Present'
+    #   DependsOn                       = "[ADOrganizationalUnit]Demo"
+    # }
     
-    ADOrganizationalUnit 'Servers' {
-      Name                            = "Servers"
-      Path                            = "OU=Demo,$domainDN"
-      ProtectedFromAccidentalDeletion = $true
-      Description                     = "Servers OU"
-      Ensure                          = 'Present'
-      DependsOn                       = "[ADOrganizationalUnit]Demo"
-    }		
-    
-    # ADUser	 'svc_sql' {
-    #   UserName              = 'svc_sql'
-    #   Description           = "Service account for SQL"
-    #   Credential            = $Cred
-    #   PasswordNotRequired   = $true
-    #   DomainName            = 'MTH-Consulting.dk'
-    #   Path                  = "OU=ServiceAccounts,OU=Demo,$domainDN"
-    #   Ensure                = 'Present'
-    #   DependsOn             = "[ADOrganizationalUnit]ServiceAccounts"
-    #   Enabled               = $true
-    #   UserPrincipalName     = "svc_sql@MTH-Consulting.dk"
-    #   PasswordNeverExpires  = $true
-    #   ChangePasswordAtLogon = $false
+    # ADOrganizationalUnit 'Servers' {
+    #   Name                            = "Servers"
+    #   Path                            = "OU=Demo,$domainDN"
+    #   ProtectedFromAccidentalDeletion = $true
+    #   Description                     = "Servers OU"
+    #   Ensure                          = 'Present'
+    #   DependsOn                       = "[ADOrganizationalUnit]Demo"
     # }
   }
 }
