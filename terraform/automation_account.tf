d5d98ce443a965d602d393938d9a6999008222c0
David Yeo <david.j.yeo@outlook.com>
2024-07-24 08:04:26 +0100

    build(dsc): Automation Account Credential

--- terraform/automation_account.tf
+++ terraform/automation_account.tf
@@ -49,35 +49,3 @@ resource "azurerm_automation_dsc_configuration" "domain" {
   location                = azurerm_resource_group.rg.location
   content_embedded        = file("../scripts/dc01.ps1")
 }
-
-resource "azurerm_automation_dsc_nodeconfiguration" "example" {
-  name                    = "test.localhost"
-  resource_group_name     = azurerm_resource_group.example.name
-  automation_account_name = azurerm_automation_account.example.name
-  depends_on              = [azurerm_automation_dsc_configuration.example]
-
-  content_embedded = <<mofcontent
-instance of MSFT_FileDirectoryConfiguration as $MSFT_FileDirectoryConfiguration1ref
-{
-  ResourceID = "[File]bla";
-  Ensure = "Present";
-  Contents = "bogus Content";
-  DestinationPath = "c:\\bogus.txt";
-  ModuleName = "PSDesiredStateConfiguration";
-  SourceInfo = "::3::9::file";
-  ModuleVersion = "1.0";
-  ConfigurationName = "bla";
-};
-instance of OMI_ConfigurationDocument
-{
-  Version="2.0.0";
-  MinimumCompatibleVersion = "1.0.0";
-  CompatibleVersionAdditionalProperties= {"Omi_BaseResource:ConfigurationName"};
-  Author="bogusAuthor";
-  GenerationDate="06/15/2018 14:06:24";
-  GenerationHost="bogusComputer";
-  Name="test";
-};
-mofcontent
-
-}
