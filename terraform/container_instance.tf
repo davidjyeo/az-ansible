2180fab4e92bbf973c9e9c0e857a67aca07c008e
David Yeo <david.j.yeo@outlook.com>
2024-07-29 09:58:24 +0100

    build(container): Registry and group

--- terraform/container_instance.tf
+++ terraform/container_instance.tf
@@ -8,8 +8,45 @@ module "avm-res-containerinstance-containergroup" {
   source              = "Azure/avm-res-containerinstance-containergroup/azurerm"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  name                = module.naming.container_group.name
+  name                = replace("${module.naming.container_group.name}", "/-/", "")
   os_type             = "linux"
   restart_policy      = "Always"
   enable_telemetry    = var.enable_telemetry
+
+  containers = {
+    container1 = {
+      name   = "control"
+      image  = "ubuntu:latest"
+      cpu    = "1"
+      memory = "2"
+      ports = [
+        {
+          port     = 22
+          protocol = "TCP"
+        }
+      ]
+      # environment_variables = {
+      #   "ENVIRONMENT" = "production"
+      # }
+      # secure_environment_variables = {
+      #   "SECENV" = "avmpoc"
+      # }
+      # volumes = {
+      #   secrets = {
+      #     mount_path = "/etc/secrets"
+      #     name       = "secret1"
+      #     secret = {
+      #       "password" = base64encode("password123")
+      #     }
+      #   },
+      #   nginx = {
+      #     mount_path = "/usr/share/nginx/html"
+      #     name       = "nginx"
+      #     secret = {
+      #       "indexpage" = base64encode("Hello, World!")
+      #     }
+      #   }
+      # }
+    }
+  }
 }
