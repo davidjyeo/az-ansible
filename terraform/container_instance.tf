5ca7d3e68ed4f2541dd0c0513506a83845ce300c
David Yeo <david.j.yeo@outlook.com>
2024-07-29 10:20:48 +0100

    build(container): ubuntu instance

--- terraform/container_instance.tf
+++ terraform/container_instance.tf
@@ -1,17 +1,20 @@
 module "avm-res-containerregistry-registry" {
   source              = "Azure/avm-res-containerregistry-registry/azurerm"
   resource_group_name = azurerm_resource_group.rg.name
-  name                = module.naming.container_registry.name
+  name                = replace("${module.naming.container_registry.name}", "/-/", "")
 }
+
+#acrans-adv-uks-01
 
 module "avm-res-containerinstance-containergroup" {
   source              = "Azure/avm-res-containerinstance-containergroup/azurerm"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  name                = replace("${module.naming.container_group.name}", "/-/", "")
-  os_type             = "linux"
-  restart_policy      = "Always"
-  enable_telemetry    = var.enable_telemetry
+  name                = module.naming.container_group.name
+  # name                = replace("${module.naming.container_group.name}", "/-/", "")
+  os_type          = "Linux"
+  restart_policy   = "Always"
+  enable_telemetry = var.enable_telemetry
 
   subnet_ids = [azurerm_subnet.ansible_subnet.id]
 
